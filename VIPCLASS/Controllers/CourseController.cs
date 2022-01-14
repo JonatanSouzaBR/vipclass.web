379f52d97f9a774ff38873f2f3abd7eeda2856d5
coepedepano <36484253+JonatanSouzaBR@users.noreply.github.com>
2022-01-13 22:35:42 -0300

    Criaçao da tela de cadastro

--- VIPCLASS/Controllers/CourseController.cs
+++ VIPCLASS/Controllers/CourseController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using VIPCLASS.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,25 +13,35 @@ namespace VIPCLASS.Controllers
 {
     public class CourseController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        [Obsolete]
+        private IHostingEnvironment _env;
+
+        [Obsolete]
+        public CourseController(IHostingEnvironment env)
         {
-            return View();
+            _env = env;
         }
 
-        public IActionResult Start()
-        {
-            return View();
-        }
+        public IActionResult Start() => View();
 
-        public IActionResult Single()
-        {
-            return View();
-        }
+        public IActionResult Single() => View();
 
-        public IActionResult Multiple()
+        public IActionResult Multiple() => View();
+
+        [RequestFormLimits(ValueLengthLimit = int.MaxValue, MultipartBodyLengthLimit = int.MaxValue)]
+        [DisableRequestSizeLimit]
+        [Consumes("multipart/form-data")]
+        [HttpPost]
+        [Obsolete]
+        public async Task<IActionResult> CreateSingleCourse(SingleCourseViewModel model)
         {
-            return View();
+            var dir = _env.ContentRootPath;
+
+            using var filestream = new FileStream(Path.Combine(dir, model.File.FileName), FileMode.Create, FileAccess.Write);
+
+            await model.File.CopyToAsync(filestream);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ VIPCLASS/ViewModels/SingleCourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace VIPCLASS.ViewModels
+{
+    public class SingleCourseViewModel
+    {
+        public IFormFile File { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
